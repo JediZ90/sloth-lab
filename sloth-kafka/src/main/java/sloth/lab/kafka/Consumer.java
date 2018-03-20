5d59ac8d06e8ebbbb4337a5419d60325a90e0ce6
炎川 <yanchuan2026@163.com>
2018-03-20 21:17:44 +0800

    Update

--- sloth-kafka/src/main/java/sloth/lab/kafka/Consumer.java
+++ sloth-kafka/src/main/java/sloth/lab/kafka/Consumer.java
@@ -3,44 +3,34 @@ package sloth.lab.kafka;
 import java.util.Arrays;
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class Consumer {
 
     @SuppressWarnings("resource")
     public static void consume(String brokers, String groupId) {
-        // Create a consumer
-        KafkaConsumer<String, String> consumer;
-        // Configure the consumer
+        // 配置consumer
         Properties properties = new Properties();
-        // Point it to the brokers
-        properties.setProperty("bootstrap.servers", brokers);
-        // Set the consumer group (all consumers must belong to a group).
-        properties.setProperty("group.id", groupId);
-        // Set how to serialize key/value pairs
-        properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        // When a group is first created, it has no offset stored to start reading from.
-        // This tells it to start
-        // with the earliest record in the stream.
-        properties.setProperty("auto.offset.reset", "earliest");
-        consumer = new KafkaConsumer<>(properties);
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
 
-        // Subscribe to the 'test' topic
+        // 订阅 'test' topic
         consumer.subscribe(Arrays.asList("test"));
 
-        // Loop until ctrl + c
         int count = 0;
         while (true) {
-            // Poll for records
             ConsumerRecords<String, String> records = consumer.poll(200);
-            // Did we get any?
             if (records.count() == 0) {
                 // timeout/nothing to read
             } else {
-                // Yes, loop over records
                 for (ConsumerRecord<String, String> record : records) {
                     // Display record and count
                     count += 1;
--- sloth-kafka/src/main/java/sloth/lab/kafka/Producer.java
+++ sloth-kafka/src/main/java/sloth/lab/kafka/Producer.java
@@ -5,31 +5,27 @@ import java.util.Properties;
 import java.util.Random;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public class Producer {
 
     public static void produce(String brokers) throws IOException {
-        // Set properties used to configure the producer
+        // 配置 producer
         Properties properties = new Properties();
-        // Set the brokers (bootstrap servers)
-        properties.setProperty("bootstrap.servers", brokers);
-        // Set how to serialize key/value pairs
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        // So we can generate random sentences
         Random random = new Random();
         String[] sentences = new String[] { "the cow jumped over the moon", "an apple a day keeps the doctor away", "four score and seven years ago", "snow white and the seven dwarfs", "i am at two with nature" };
 
         String progressAnimation = "|/-\\";
-        // Produce a bunch of records
+
         for (int i = 0; i < 1000000; i++) {
-            // Pick a sentence at random
             String sentence = sentences[random.nextInt(sentences.length)];
-            // Send the sentence to the test topic
             producer.send(new ProducerRecord<String, String>("test", sentence));
             String progressBar = "\r" + progressAnimation.charAt(i % progressAnimation.length()) + " " + i;
             System.out.write(progressBar.getBytes());
